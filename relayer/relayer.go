fdf828f0fd0a6f929f46eea57a71395ab123d2eb
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-06-22 15:54:34 -0400

    relayer.RpcRelayer, stubbing the struct adhereing to Relayer interface

--- relayer/relayer.go
+++ relayer/relayer.go
@@ -1,5 +1,1 @@
 package relayer
-
-// TODO: proto gen client goes here..
-
-// TODO: lets add RpcRelayer implementation
---
+++ relayer/rpc_relayer.go
@@ -0,0 +1,46 @@
+package relayer
+
+import (
+	"context"
+	"math/big"
+	"time"
+
+	"github.com/0xsequence/ethkit/ethrpc"
+	"github.com/0xsequence/ethkit/ethtxn"
+	"github.com/0xsequence/ethkit/go-ethereum/core/types"
+	"github.com/0xsequence/go-sequence"
+)
+
+// TODO ....
+type RpcRelayer struct {
+}
+
+var _ sequence.Relayer = &RpcRelayer{}
+
+func (r *RpcRelayer) GetProvider() *ethrpc.Provider {
+	return nil
+}
+
+// ..
+func (r *RpcRelayer) EstimateGasLimits(ctx context.Context, walletConfig sequence.WalletConfig, walletContext sequence.WalletContext, transactions sequence.Transactions) (sequence.Transactions, error) {
+	return nil, nil
+}
+
+// NOTE: nonce space is 160 bits wide
+func (r *RpcRelayer) GetNonce(ctx context.Context, walletConfig sequence.WalletConfig, walletContext sequence.WalletContext, space *big.Int, blockNum *big.Int) (*big.Int, error) {
+	return nil, nil
+}
+
+// Relay will submit the Sequence signed meta transaction to the relayer. The method will block until the relayer
+// responds with the native transaction hash (*types.Transaction), which means the relayer has submitted the transaction
+// request to the network. Clients can use WaitReceipt to wait until the metaTxnID has been mined.
+//
+// TODO: ethwallet.WaitReceipt needs to move to ethtxn or ethrpc ..
+func (r *RpcRelayer) Relay(ctx context.Context, signedTxs *sequence.SignedTransactions) (sequence.MetaTxnID, *types.Transaction, ethtxn.WaitReceipt, error) {
+	return "", nil, nil, nil
+}
+
+// ..
+func (r *RpcRelayer) Wait(ctx context.Context, metaTxID sequence.MetaTxnID, timeout time.Duration) (*types.Receipt, error) {
+	return nil, nil
+}
