fe2d5481e7e491ff9e74af7bdd56d1b0d183a7dd
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-06-23 11:04:20 -0400

    move test helpers to helpers_test.go

--- transactions_test.go
+++ transactions_test.go
@@ -1,14 +1,11 @@
 package sequence_test
 
 import (
-	"context"
 	"encoding/hex"
 	"math/big"
 	"testing"
 
 	"github.com/0xsequence/ethkit/ethcoder"
-	"github.com/0xsequence/ethkit/go-ethereum/common"
-	"github.com/0xsequence/ethkit/go-ethereum/core/types"
 	"github.com/0xsequence/go-sequence"
 	"github.com/0xsequence/go-sequence/testutil"
 	"github.com/stretchr/testify/assert"
@@ -43,7 +40,7 @@ func TestERC20Transfer(t *testing.T) {
 	assert.NotNil(t, wallets)
 
 	// Create normal txn of: callmockContract.mockMint(wallets[0], 100)
-	callmockContract := testChain.UniDeploy(t, "ERC20Mock", 0)
+	callmockContract, _ := testChain.Deploy(t, "ERC20Mock")
 	calldata, err := callmockContract.Encode("mockMint", wallets[0].Address(), big.NewInt(100))
 	assert.NoError(t, err)
 
@@ -107,69 +104,6 @@ func TestERC20Transfer(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Len(t, ret, 1)
 	assert.Equal(t, "30", ret[0])
-}
-
-func signAndSend(t *testing.T, wallet *sequence.Wallet, to common.Address, data []byte) error {
-	stx := &sequence.Transaction{
-		// DelegateCall:  false,
-		// RevertOnError: false,
-		// GasLimit: big.NewInt(800000),
-		// Value:         big.NewInt(0),
-		To:   to,
-		Data: data,
-	}
-
-	// Now, we must sign the meta txn
-	signedTx, err := wallet.SignTransaction(context.Background(), stx)
-	assert.NoError(t, err)
-
-	metaTxnID, tx, waitReceipt, err := wallet.SendTransaction(context.Background(), signedTx)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, metaTxnID)
-	assert.NotNil(t, tx)
-
-	receipt, err := waitReceipt(context.Background())
-	assert.NoError(t, err)
-	assert.True(t, receipt.Status == types.ReceiptStatusSuccessful)
-
-	// TODO: decode the receipt, and lets confirm we have the metaTxnID event in there..
-	// NOTE: if you start test chain with `make start-test-chain-verbose`, you will see the metaTxnID above
-	// correctly logged..
-
-	return err
-}
-
-func batchSignAndSend(t *testing.T, wallet *sequence.Wallet, to common.Address, data [][]byte) error {
-	var stxs []*sequence.Transaction
-	for i := 0; i < len(data); i++ {
-		stxs = append(stxs, &sequence.Transaction{
-			// DelegateCall:  false,
-			// RevertOnError: false,
-			// GasLimit: big.NewInt(800000),
-			// Value:         big.NewInt(0),
-			To:   to,
-			Data: data[i],
-		})
-	}
-
-	// Now, we must sign the meta txn
-	signedTx, err := wallet.SignTransactions(context.Background(), stxs)
-	assert.NoError(t, err)
-
-	metaTxnID, tx, waitReceipt, err := wallet.SendTransactions(context.Background(), signedTx)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, metaTxnID)
-	assert.NotNil(t, tx)
-
-	receipt, err := waitReceipt(context.Background())
-	assert.NoError(t, err)
-	assert.True(t, receipt.Status == types.ReceiptStatusSuccessful)
-
-	// TODO: decode the receipt, and lets confirm we have the metaTxnID event in there..
-	// NOTE: if you start test chain with `make start-test-chain-verbose`, you will see the metaTxnID above
-	// correctly logged..
-
-	return err
 }
 
 func TestDecodeExecute(t *testing.T) {
