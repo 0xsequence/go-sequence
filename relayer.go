1ee41e89c6daa3227f1a53566a563689a4e0900b
Agustin Aguilar <Agusxrun@gmail.com>
2021-06-24 16:35:50 +0000

    Implement WaitForMetaTxn

--- relayer.go
+++ relayer.go
@@ -1,6 +1,7 @@
 package sequence
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"math/big"
@@ -9,6 +10,7 @@ import (
 	"github.com/0xsequence/ethkit/ethcoder"
 	"github.com/0xsequence/ethkit/ethrpc"
 	"github.com/0xsequence/ethkit/ethtxn"
+	"github.com/0xsequence/ethkit/go-ethereum"
 	"github.com/0xsequence/ethkit/go-ethereum/common"
 	"github.com/0xsequence/ethkit/go-ethereum/core/types"
 	"github.com/0xsequence/go-sequence/contracts"
@@ -37,6 +39,11 @@ type Relayer interface {
 }
 
 type MetaTxnID string
+
+var (
+	txFailedTopic     = common.Hex2Bytes("3dbd1590ea96dd3253a91f24e64e3a502e1225d602a5731357bc12643070ccd7")
+	nonceChangedTopic = common.HexToHash("1f180c27086c7a39ea2a7b25239d1ab92348f07ca7bb59d1438fcf527568f881")
+)
 
 func ComputeMetaTxnID(walletAddress common.Address, chainID *big.Int, txns Transactions) (MetaTxnID, error) {
 	txnsDigest, err := txns.Digest()
@@ -86,6 +93,65 @@ func EncodeTransactionsForRelaying(relayer Relayer, walletConfig WalletConfig, w
 }
 
 func WaitForMetaTxn(ctx context.Context, provider *ethrpc.Provider, metaTxnID MetaTxnID, timeout time.Duration) (*types.Receipt, error) {
-	// TODO.
-	return nil, fmt.Errorf("TODO")
+	timeoutTime := time.Now().Add(timeout)
+
+	// Start listening logs from current block - 1024
+	block, err := provider.BlockNumber(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	lastBlockNumber := block - 1024
+
+	metaTxIdBytes := common.Hex2Bytes(string(metaTxnID))
+
+	// All transactions must change nonces
+	// so load all nonce changes and search the logs
+	nonceChangedTopics := [][]common.Hash{{nonceChangedTopic}}
+
+	// Load all logs until we found the receipt or we reach timeout
+	for time.Now().Before(timeoutTime) {
+		block, err := provider.BlockNumber(ctx)
+
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		query := ethereum.FilterQuery{
+			FromBlock: big.NewInt(int64(lastBlockNumber) - 12),
+			ToBlock:   big.NewInt(int64(block)),
+			Topics:    nonceChangedTopics,
+		}
+
+		logs, err := provider.FilterLogs(ctx, query)
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		for _, log := range logs {
+			tx, err := provider.TransactionReceipt(ctx, log.TxHash)
+			if err != nil {
+				time.Sleep(time.Second)
+				continue
+			}
+
+			for _, txLog := range tx.Logs {
+				// Success transactions have no topics and the metaTxId is the data
+				found := len(txLog.Topics) == 0 && bytes.Equal(txLog.Data, metaTxIdBytes)
+				// Failed transactions have the TxFailed topic and the data begins with the metaTxInd
+				found = found || (len(txLog.Topics) == 1 && bytes.Equal(txLog.Topics[0].Bytes(), txFailedTopic) && bytes.HasPrefix(txLog.Data, metaTxIdBytes))
+
+				if found {
+					return tx, nil
+				}
+			}
+		}
+
+		time.Sleep(time.Second)
+		lastBlockNumber = block
+	}
+
+	return nil, fmt.Errorf("Waiting for meta transaction timeout %v", metaTxnID)
 }
---
+++ relayer_test.go
@@ -0,0 +1,103 @@
+package sequence_test
+
+import (
+	"context"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/0xsequence/ethkit/ethcoder"
+	"github.com/0xsequence/go-sequence"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetReceiptOfTransaction(t *testing.T) {
+	// Ensure dummy sequence wallet from seed 1 is deployed
+	wallet, err := testChain.DummySequenceWallet(1)
+	assert.NoError(t, err)
+	assert.NotNil(t, wallet)
+
+	isDeployed, err := wallet.IsDeployed()
+	assert.NoError(t, err)
+	assert.True(t, isDeployed)
+
+	// Create normal txn of: callmockContract.testCall(55, 0x112255)
+	callmockContract := testChain.UniDeploy(t, "WALLET_CALL_RECV_MOCK", 0)
+	calldata, err := callmockContract.Encode("testCall", big.NewInt(55), ethcoder.MustHexDecode("0x112255"))
+	assert.NoError(t, err)
+
+	nonce, err := wallet.GetNonce()
+	assert.NoError(t, err)
+
+	stx := &sequence.Transaction{
+		To:            callmockContract.Address,
+		Data:          calldata,
+		Value:         big.NewInt(0),
+		GasLimit:      big.NewInt(190000),
+		DelegateCall:  false,
+		RevertOnError: false,
+		Nonce:         nonce,
+	}
+
+	err = signAndSendRawTransaction(t, wallet, stx)
+	assert.NoError(t, err)
+
+	// Get transactions digest
+	metaTxnId, err := sequence.ComputeMetaTxnID(wallet.Address(), testChain.ChainID(), stx.Bundle())
+	assert.NoError(t, err)
+	assert.NotEmpty(t, metaTxnId)
+
+	// Find receipt
+	receipt, err := sequence.WaitForMetaTxn(context.Background(), testChain.Provider, metaTxnId, 3*time.Second)
+	assert.NoError(t, err)
+	assert.NotNil(t, receipt)
+}
+
+func TestGetReceiptOfErrorTransaction(t *testing.T) {
+	// Ensure dummy sequence wallet from seed 1 is deployed
+	wallet, err := testChain.DummySequenceWallet(1)
+	assert.NoError(t, err)
+	assert.NotNil(t, wallet)
+
+	isDeployed, err := wallet.IsDeployed()
+	assert.NoError(t, err)
+	assert.True(t, isDeployed)
+
+	// Turn on revert flag on callmock
+	callmockContract, _ := testChain.Deploy(t, "WALLET_CALL_RECV_MOCK")
+	calldata, err := callmockContract.Encode("setRevertFlag", true)
+	assert.NoError(t, err)
+
+	err = signAndSend(t, wallet, callmockContract.Address, calldata)
+	assert.NoError(t, err)
+
+	// Call callmock, this should revert and fail the transaction
+	calldata, err = callmockContract.Encode("testCall", big.NewInt(55), ethcoder.MustHexDecode("0x112255"))
+	assert.NoError(t, err)
+
+	nonce, err := wallet.GetNonce()
+	assert.NoError(t, err)
+
+	stx := &sequence.Transaction{
+		To:            callmockContract.Address,
+		Data:          calldata,
+		Value:         big.NewInt(0),
+		GasLimit:      big.NewInt(190000),
+		DelegateCall:  false,
+		RevertOnError: false,
+		Nonce:         nonce,
+	}
+
+	err = signAndSendRawTransaction(t, wallet, stx)
+	assert.NoError(t, err)
+
+	// Get transactions digest
+	metaTxnId, err := sequence.ComputeMetaTxnID(wallet.Address(), testChain.ChainID(), stx.Bundle())
+	assert.NoError(t, err)
+	assert.NotEmpty(t, metaTxnId)
+
+	// Find receipt
+	receipt, err := sequence.WaitForMetaTxn(context.Background(), testChain.Provider, metaTxnId, 3*time.Second)
+	assert.NoError(t, err)
+	assert.NotNil(t, receipt)
+}
