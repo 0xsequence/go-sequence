8ca777e14c17db9a8498c08842646793fe8271ef
William Hua <william@attente.ca>
2021-09-28 16:38:38 -0400

    contracts: yarn gen

--- contracts/gen/gasestimator/gas_estimator.gen.go
+++ contracts/gen/gasestimator/gas_estimator.gen.go
@@ -4,6 +4,7 @@
 package gasestimator
 
 import (
+	"errors"
 	"math/big"
 	"strings"
 
@@ -17,6 +18,7 @@ import (
 
 // Reference imports to suppress errors if they are not otherwise used.
 var (
+	_ = errors.New
 	_ = big.NewInt
 	_ = strings.NewReader
 	_ = ethereum.NotFound
@@ -26,20 +28,31 @@ var (
 	_ = event.NewSubscription
 )
 
+// GasEstimatorMetaData contains all meta data concerning the GasEstimator contract.
+var GasEstimatorMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_to\",\"type\":\"address\"},{\"internalType\":\"bytes\",\"name\":\"_data\",\"type\":\"bytes\"}],\"name\":\"estimate\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"success\",\"type\":\"bool\"},{\"internalType\":\"bytes\",\"name\":\"result\",\"type\":\"bytes\"},{\"internalType\":\"uint256\",\"name\":\"gas\",\"type\":\"uint256\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+	Bin: "0x608060405234801561001057600080fd5b50610208806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c80630eb34cd314610030575b600080fd5b6100bd6004803603604081101561004657600080fd5b73ffffffffffffffffffffffffffffffffffffffff823516919081019060408101602082013564010000000081111561007e57600080fd5b82018360208201111561009057600080fd5b803590602001918460018302840111640100000000831117156100b257600080fd5b509092509050610145565b60405180841515815260200180602001838152602001828103825284818151815260200191508051906020019080838360005b838110156101085781810151838201526020016100f0565b50505050905090810190601f1680156101355780820380516001836020036101000a031916815260200191505b5094505050505060405180910390f35b600060606000805a90508673ffffffffffffffffffffffffffffffffffffffff168686604051808383808284376040519201945060009350909150508083038183865af19150503d80600081146101b8576040519150601f19603f3d011682016040523d82523d6000602084013e6101bd565b606091505b5090945092505a81039150509350935093905056fea26469706673582212202294382602e928b6c962b89f8f706713a73b32b446a191ebcfa9b77f7004d19064736f6c63430007060033",
+}
+
 // GasEstimatorABI is the input ABI used to generate the binding from.
-const GasEstimatorABI = "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_to\",\"type\":\"address\"},{\"internalType\":\"bytes\",\"name\":\"_data\",\"type\":\"bytes\"}],\"name\":\"estimate\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"success\",\"type\":\"bool\"},{\"internalType\":\"bytes\",\"name\":\"result\",\"type\":\"bytes\"},{\"internalType\":\"uint256\",\"name\":\"gas\",\"type\":\"uint256\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+// Deprecated: Use GasEstimatorMetaData.ABI instead.
+var GasEstimatorABI = GasEstimatorMetaData.ABI
 
 // GasEstimatorBin is the compiled bytecode used for deploying new contracts.
-const GasEstimatorBin = "0x608060405234801561001057600080fd5b50610208806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c80630eb34cd314610030575b600080fd5b6100bd6004803603604081101561004657600080fd5b73ffffffffffffffffffffffffffffffffffffffff823516919081019060408101602082013564010000000081111561007e57600080fd5b82018360208201111561009057600080fd5b803590602001918460018302840111640100000000831117156100b257600080fd5b509092509050610145565b60405180841515815260200180602001838152602001828103825284818151815260200191508051906020019080838360005b838110156101085781810151838201526020016100f0565b50505050905090810190601f1680156101355780820380516001836020036101000a031916815260200191505b5094505050505060405180910390f35b600060606000805a90508673ffffffffffffffffffffffffffffffffffffffff168686604051808383808284376040519201945060009350909150508083038183865af19150503d80600081146101b8576040519150601f19603f3d011682016040523d82523d6000602084013e6101bd565b606091505b5090945092505a81039150509350935093905056fea26469706673582212202294382602e928b6c962b89f8f706713a73b32b446a191ebcfa9b77f7004d19064736f6c63430007060033"
+// Deprecated: Use GasEstimatorMetaData.Bin instead.
+var GasEstimatorBin = GasEstimatorMetaData.Bin
 
 // DeployGasEstimator deploys a new Ethereum contract, binding an instance of GasEstimator to it.
 func DeployGasEstimator(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *GasEstimator, error) {
-	parsed, err := abi.JSON(strings.NewReader(GasEstimatorABI))
+	parsed, err := GasEstimatorMetaData.GetAbi()
 	if err != nil {
 		return common.Address{}, nil, nil, err
 	}
+	if parsed == nil {
+		return common.Address{}, nil, nil, errors.New("GetABI returned nil")
+	}
 
-	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(GasEstimatorBin), backend)
+	address, tx, contract, err := bind.DeployContract(auth, *parsed, common.FromHex(GasEstimatorBin), backend)
 	if err != nil {
 		return common.Address{}, nil, nil, err
 	}
