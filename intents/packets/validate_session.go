4291ede195b2b9cdfe7f094462b54bfccd120006
Agusx1211 <agusgit@pm.me>
2023-11-07 08:45:08 +0000

    Add get session packet

--- intents/packets/validate_session.go
+++ intents/packets/validate_session.go
@@ -39,3 +39,35 @@ type ValidateSessionPacketResponse struct {
 		SmsSent   bool `json:"smsSent"`
 	}
 }
+
+const GetSessionPacketCode = "getSession"
+
+type GetSessionPacket struct {
+	BasePacketForWallet
+	Session string `json:"session"`
+}
+
+func (p *GetSessionPacket) Unmarshal(i *intents.Intent) error {
+	err := json.Unmarshal(i.Packet, &p)
+	if err != nil {
+		return err
+	}
+
+	if p.Code != GetSessionPacketCode {
+		return fmt.Errorf("packet code is not '%s', got '%s'", GetSessionPacketCode, p.Code)
+	}
+
+	return nil
+}
+
+const GetSessionPacketResponseCode = "getSessionResponse"
+
+type GetSessionPacketResponse struct {
+	Code string `json:"code"`
+
+	Data struct {
+		Session   string `json:"session"`
+		Wallet    string `json:"wallet"`
+		Validated bool   `json:"validated"`
+	}
+}
