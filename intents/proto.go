f87642d47b4cf29b1cd0f91867431a100ee61582
Patryk Kalinowski <patryk@kalinowski.dev>
2024-02-13 12:12:51 +0100

    Add missing delayedEncode transaction type

--- intents/proto.go
+++ intents/proto.go
@@ -1,4 +1,4 @@
-// Server
+// Client
 //go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=intent.ridl -target=golang -pkg=intents -client -out=./intent.gen.go
 //go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=intent.ridl -target=typescript -client -out=./intent.gen.ts
 
