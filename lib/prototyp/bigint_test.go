66e5170d370beeb0a6866f24239ef5eb2a4a11e2
balaji <rbalajis25@gmail.com>
2024-02-14 19:24:23 +0530

    add test coverage

--- lib/prototyp/bigint_test.go
+++ lib/prototyp/bigint_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBigIntJSONMarshalling(t *testing.T) {
@@ -50,11 +51,17 @@ func TestBigIntScan(t *testing.T) {
 	b := BigInt{}
 
 	assert.NoError(t, b.Scan("1"))
+	require.Equal(t, b.Int64(), int64(1))
 	assert.NoError(t, b.Scan([]byte("1")))
+	require.Equal(t, b.Int64(), int64(1))
 	assert.Error(t, b.Scan(1))
+	require.Equal(t, b.Int64(), int64(1))
 	assert.Error(t, b.Scan(1.0))
+	require.Equal(t, b.Int64(), int64(1))
 	assert.Error(t, b.Scan("1x"))
+	require.Equal(t, b.Int64(), int64(1))
 	assert.Error(t, b.Scan("1."))
+	require.Equal(t, b.Int64(), int64(1))
 }
 
 func TestBigIntInvalidInput(t *testing.T) {
@@ -69,4 +76,172 @@ func TestBigIntInvalidInput(t *testing.T) {
 	b = NewBigIntFromHexString("/0xaBc-$$2Efg")
 	assert.Equal(t, int64(0xaBc2Ef), b.Int64())
 
+	b = NewBigIntFromString("1234", 10)
+	assert.Equal(t, int64(1234), b.Int64())
+}
+
+func TestToBigInt(t *testing.T) {
+	a := big.NewInt(1)
+
+	// copies  a
+	b := ToBigInt(a)
+	c := ToBigInt(a)
+
+	// not mutating b or a
+	c.Sub(b.Int())
+
+	require.Equal(t, int64(1), b.Int64())
+	require.Equal(t, int64(0), c.Int64())
+}
+
+func TestSetString(t *testing.T) {
+	a := NewBigInt(1)
+
+	ok := a.SetString("12", 10)
+	require.True(t, ok)
+	require.Equal(t, int64(12), a.Int64())
+}
+
+func TestEqual(t *testing.T) {
+	a := NewBigInt(10)
+	b := NewBigInt(10)
+	require.True(t, b.Equals(a.Int()))
+}
+
+func TestBigIntArray(t *testing.T) {
+	input := []int64{1, 2, 3, 4}
+
+	a := []*big.Int{}
+	for _, i := range input {
+		a = append(a, big.NewInt(i))
+	}
+
+	b := ToBigIntArray(a)
+	for i := range input {
+		require.Equal(t, a[i].Int64(), b[i].Int64())
+	}
+}
+
+func TestToBigIntArrayFromStringArray(t *testing.T) {
+	input := []string{"10", "12", "123", "562"}
+
+	expected := []BigInt{}
+	for _, i := range input {
+		expected = append(expected, NewBigIntFromNumberString(i))
+	}
+
+	got, err := ToBigIntArrayFromStringArray(input, 10)
+	require.NoError(t, err)
+	require.Equal(t, expected, got)
+}
+
+func TestToBigIntFromInt64(t *testing.T) {
+	got := ToBigIntFromInt64(10)
+	expected := NewBigInt(10)
+
+	require.Equal(t, expected, got)
+}
+
+func TestString(t *testing.T) {
+	a := NewBigInt(10)
+	require.Equal(t, "10", a.String())
+}
+
+func TestAdd(t *testing.T) {
+	AddArgs := [][]*big.Int{
+		// x,y,z
+		{big.NewInt(1), big.NewInt(2), big.NewInt(3)},
+		{big.NewInt(3), big.NewInt(2), big.NewInt(5)},
+		{big.NewInt(123), big.NewInt(5), big.NewInt(128)},
+		{big.NewInt(1), big.NewInt(200), big.NewInt(201)},
+		{big.NewInt(-100), big.NewInt(200), big.NewInt(100)},
+		{big.NewInt(100), big.NewInt(-200), big.NewInt(-100)},
+	}
+
+	for _, arg := range AddArgs {
+		x := ToBigInt(arg[0])
+		y := ToBigInt(arg[1])
+		x.Add(y.Int())
+		require.Equal(t, x.Int(), arg[2])
+	}
+}
+
+func TestComparison(t *testing.T) {
+	type Cases struct {
+		A        BigInt
+		B        *big.Int
+		Expected bool
+	}
+
+	greater := []Cases{{
+		A:        NewBigInt(10),
+		B:        big.NewInt(1),
+		Expected: true,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(20),
+		Expected: false,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(10),
+		Expected: false,
+	}}
+
+	for _, test := range greater {
+		require.Equal(t, test.Expected, test.A.Gt(test.B))
+	}
+
+	greaterEq := []Cases{{
+		A:        NewBigInt(10),
+		B:        big.NewInt(1),
+		Expected: true,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(20),
+		Expected: false,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(10),
+		Expected: true,
+	}}
+
+	for _, test := range greaterEq {
+		require.Equal(t, test.Expected, test.A.Gte(test.B))
+	}
+
+	lesser := []Cases{{
+		A:        NewBigInt(10),
+		B:        big.NewInt(1),
+		Expected: false,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(20),
+		Expected: true,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(10),
+		Expected: false,
+	}}
+
+	for _, test := range lesser {
+		require.Equal(t, test.Expected, test.A.Lt(test.B))
+	}
+
+	lesserEq := []Cases{{
+		A:        NewBigInt(10),
+		B:        big.NewInt(1),
+		Expected: false,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(20),
+		Expected: true,
+	}, {
+		A:        NewBigInt(10),
+		B:        big.NewInt(10),
+		Expected: true,
+	}}
+
+	for _, test := range lesserEq {
+		require.Equal(t, test.Expected, test.A.Lte(test.B))
+	}
 }
