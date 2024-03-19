85c3d2496b6d12f6b613c945decaa7b8dab98851
Marcin Gorzynski <marcin@gorzynski.me>
2024-03-19 12:34:25 +0100

    intents: IntentResponseTyped - improvement

--- intents/intent_response_typed.go
+++ intents/intent_response_typed.go
@@ -5,34 +5,78 @@ import (
 	"fmt"
 )
 
+func IntentResponseTypeToCode[T any](t *T) string {
+	var data any = t
+	switch data.(type) {
+	case *IntentResponseSessionOpened:
+		return IntentResponseCodeSessionOpened
+	case *IntentResponseValidationStarted:
+		return IntentResponseCodeValidationStarted
+	case *IntentResponseValidationFinished:
+		return IntentResponseCodeValidationFinished
+	case *IntentResponseSignedMessage:
+		return IntentResponseCodeSignedMessage
+	case *IntentResponseFeeOptions:
+		return IntentResponseCodeFeeOptions
+	case *IntentResponseTransactionReceipt:
+		return IntentResponseCodeTransactionReceipt
+	case *IntentResponseTransactionFailed:
+		return IntentResponseCodeTransactionFailed
+	case *IntentResponseGetSession:
+		return IntentResponseCodeGetSessionResponse
+	default:
+		return ""
+	}
+}
+
 type IntentResponseTyped[T any] struct {
 	IntentResponse
 	Data T `json:"data"`
 }
 
+func NewIntentResponseTyped[T any](data T) *IntentResponseTyped[T] {
+	return &IntentResponseTyped[T]{
+		IntentResponse: IntentResponse{
+			Code: IntentResponseTypeToCode(&data),
+			Data: data,
+		},
+		Data: data,
+	}
+}
+
 func NewIntentResponseTypedFromIntentResponse[T any](res *IntentResponse) (*IntentResponseTyped[T], error) {
-	switch res.Code {
-	case IntentResponseCodeSessionOpened,
-		IntentResponseCodeValidationRequired,
-		IntentResponseCodeValidationStarted,
-		IntentResponseCodeValidationFinished,
-		IntentResponseCodeSignedMessage,
-		IntentResponseCodeFeeOptions,
-		IntentResponseCodeTransactionReceipt,
-		IntentResponseCodeTransactionFailed,
-		IntentResponseCodeGetSessionResponse:
-		data, err := json.Marshal(res)
+	switch res.Data.(type) {
+	case T:
+		return &IntentResponseTyped[T]{
+			IntentResponse: *res,
+			Data:           res.Data.(T),
+		}, nil
+	case map[string]any:
+		data := res.Data.(map[string]any)
+
+		// convert to json
+		dataJSON, err := json.Marshal(data)
 		if err != nil {
 			return nil, err
 		}
 
-		var resTyped IntentResponseTyped[T]
-		err = json.Unmarshal(data, &resTyped)
+		// convert to typed data
+		var typedData T
+		err = json.Unmarshal(dataJSON, &typedData)
 		if err != nil {
 			return nil, err
 		}
-		return &resTyped, nil
+
+		// check if intent response code and data type match
+		if IntentResponseTypeToCode(&typedData) != res.Code {
+			return nil, fmt.Errorf("intent response code and data type mismatch")
+		}
+
+		return &IntentResponseTyped[T]{
+			IntentResponse: *res,
+			Data:           typedData,
+		}, nil
 	default:
-		return nil, fmt.Errorf("unknown response code: %s", res.Code)
+		return nil, fmt.Errorf("invalid intent data type")
 	}
 }
