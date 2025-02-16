709e1f13b430f6019ba18d2f36419891638effea
Shun Kakinoki <shunkakinoki@gmail.com>
2025-02-16 18:04:34 +0900

    feat: ini config v2

--- intent_config_test.go
+++ intent_config_test.go
@@ -1,11 +1,13 @@
 package sequence_test
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
 	"github.com/0xsequence/ethkit/ethcoder"
 	"github.com/0xsequence/ethkit/ethwallet"
+	"github.com/0xsequence/ethkit/go-ethereum/common"
 	"github.com/0xsequence/go-sequence"
 	v2 "github.com/0xsequence/go-sequence/core/v2"
 	"github.com/davecgh/go-spew/spew"
@@ -16,9 +18,6 @@ import (
 func TestCreateIntentConfigurationSignature(t *testing.T) {
 	// Create test wallets
 	eoa1, err := ethwallet.NewWalletFromRandomEntropy()
-	require.NoError(t, err)
-
-	eoa2, err := ethwallet.NewWalletFromRandomEntropy()
 	require.NoError(t, err)
 
 	// Create a mock transaction
@@ -40,6 +39,12 @@ func TestCreateIntentConfigurationSignature(t *testing.T) {
 		signature, err := sequence.CreateIntentConfigurationSignature(eoa1.Address(), []*sequence.Transaction{stx})
 		require.NoError(t, err)
 
+		// Print the eoa1's address
+		fmt.Println("eoa1 address:", eoa1.Address())
+
+		// Dump the signature
+		spew.Dump(signature)
+
 		// Verify signature format
 		require.Equal(t, byte(0x02), signature[0], "signature should start with version byte 0x02 (NoChainID)")
 
@@ -48,6 +53,7 @@ func TestCreateIntentConfigurationSignature(t *testing.T) {
 		if node, ok := config.Tree.(*v2.WalletConfigTreeNode); ok {
 			if rightNode, ok := node.Right.(*v2.WalletConfigTreeSubdigestLeaf); ok {
 				subdigestLeaf = rightNode
+				fmt.Println("decoded subdigest leaf:", subdigestLeaf)
 			}
 		}
 		require.NotNil(t, subdigestLeaf, "config should contain a subdigest leaf")
@@ -57,13 +63,36 @@ func TestCreateIntentConfigurationSignature(t *testing.T) {
 		require.NoError(t, err, "signature should be decodable")
 
 		// Dump the sig
+		fmt.Println("DUMPING SIG")
 		spew.Dump(sig)
 
 		// Verify signature type by checking the first byte
 		require.Equal(t, byte(0x02), signature[0], "signature should be a NoChainID signature type")
 
+		// Get the `signatureTree` and print it
+		if regSig, ok := sig.(*v2.NoChainIDSignature); ok {
+			fmt.Println("DUMPING TREE")
+			spew.Dump(regSig.GetTree())
+		} else {
+			fmt.Println("signature type does not expose wallet config tree")
+		}
+
+		// Print the image hash of the subdigest
+		fmt.Println(subdigestLeaf.ImageHash())
+
+		// Print the full signature in hex
+		fmt.Println("full signature:", sig)
+
+		// Get the full signature in string
+		sigDataStr, err := sig.Data()
+		require.NoError(t, err)
+		fmt.Println("full signature hex:", common.Bytes2Hex(sigDataStr))
+
+		subdigestStr := subdigestLeaf.Subdigest.Hash.Hex()
+		fmt.Println("subdigest hex:", subdigestStr)
+
 		// Verify the signature contains the subdigest
-		require.Contains(t, signature, subdigestLeaf.Subdigest.Hash.Bytes(), "signature should contain the subdigest")
+		require.Contains(t, common.Bytes2Hex(sigDataStr), subdigestStr[2:], "signature should contain the subdigest")
 	})
 
 	t.Run("different transactions produce different signatures", func(t *testing.T) {
@@ -105,17 +134,5 @@ func TestCreateIntentConfigurationSignature(t *testing.T) {
 
 		// Verify signatures are the same
 		require.Equal(t, sig1, sig2, "same transactions should produce same signatures")
-	})
-
-	t.Run("different signers with same transactions produce same signatures", func(t *testing.T) {
-		// Create signatures with different signers but same transaction
-		sig1, err := sequence.CreateIntentConfigurationSignature(eoa1.Address(), []*sequence.Transaction{stx})
-		require.NoError(t, err)
-
-		sig2, err := sequence.CreateIntentConfigurationSignature(eoa2.Address(), []*sequence.Transaction{stx})
-		require.NoError(t, err)
-
-		// Verify signatures are the same since they're based on transaction digest
-		require.Equal(t, sig1, sig2, "signatures should be based on transaction digest only")
 	})
 }
