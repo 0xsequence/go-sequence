a649623e94b449453889dfdf954d2475d6ce5531
Shun Kakinoki <shunkakinoki@gmail.com>
2025-02-27 16:30:41 +0900

    chore: update

--- deployer/eip2470_deployer_test.go
+++ deployer/eip2470_deployer_test.go
@@ -20,10 +20,13 @@ func TestEIP2470Deployer(t *testing.T) {
 	eip2470Deployer, err := deployer.NewEIP2470Deployer(testWallet)
 	require.NoError(t, err)
 
+	testV1SequenceContext := testutil.V1SequenceContext()
+
 	// Deploy sequence wallet-contract factory and ensure it equals the expected value in testutil
 	V1WalletFactoryAddress, err := eip2470Deployer.Deploy(context.Background(), contracts.V1.WalletFactory.ABI, contracts.V1.WalletFactory.Bin, 0, nil, 1000000)
 	assert.NoError(t, err)
-	// NOTICE: v1 is deployed w/ the universal deployer, not the EIP2470 deployer
+	// v1 is deployed w/ the universal deployer, not the EIP2470 deployer
+	assert.NotEqual(t, testV1SequenceContext.FactoryAddress, V1WalletFactoryAddress)
 	assert.Equal(t, common.HexToAddress("0xf87Cb4C012256149569615642dE44Cf2373efcC9"), V1WalletFactoryAddress)
 
 	testV2SequenceContext := testutil.V2SequenceContext()
