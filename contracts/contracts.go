d6d46d3786cdcbae88e39c84c738e0f5e5fa2f0c
William Hua <william@attente.ca>
2025-02-27 16:39:16 -0500

    contracts: rename imports

--- contracts/contracts.go
+++ contracts/contracts.go
@@ -11,22 +11,22 @@ import (
 	"github.com/0xsequence/go-sequence/contracts/gen/ierc1271"
 	"github.com/0xsequence/go-sequence/contracts/gen/niftyswap"
 	"github.com/0xsequence/go-sequence/contracts/gen/tokens"
-	walletfactory1 "github.com/0xsequence/go-sequence/contracts/gen/v1/walletfactory"
-	walletgasestimator1 "github.com/0xsequence/go-sequence/contracts/gen/v1/walletgasestimator"
-	walletguest1 "github.com/0xsequence/go-sequence/contracts/gen/v1/walletguest"
-	walletmain1 "github.com/0xsequence/go-sequence/contracts/gen/v1/walletmain"
-	walletupgradable1 "github.com/0xsequence/go-sequence/contracts/gen/v1/walletupgradable"
-	walletutils1 "github.com/0xsequence/go-sequence/contracts/gen/v1/walletutils"
-	walletfactory2 "github.com/0xsequence/go-sequence/contracts/gen/v2/walletfactory"
-	walletgasestimator2 "github.com/0xsequence/go-sequence/contracts/gen/v2/walletgasestimator"
-	walletguest2 "github.com/0xsequence/go-sequence/contracts/gen/v2/walletguest"
-	walletmain2 "github.com/0xsequence/go-sequence/contracts/gen/v2/walletmain"
-	walletupgradable2 "github.com/0xsequence/go-sequence/contracts/gen/v2/walletupgradable"
-	walletutils2 "github.com/0xsequence/go-sequence/contracts/gen/v2/walletutils"
-	walletfactory3 "github.com/0xsequence/go-sequence/contracts/gen/v3/walletfactory"
-	walletguest3 "github.com/0xsequence/go-sequence/contracts/gen/v3/walletguest"
-	walletstage13 "github.com/0xsequence/go-sequence/contracts/gen/v3/walletstage1"
-	walletstage23 "github.com/0xsequence/go-sequence/contracts/gen/v3/walletstage2"
+	v1Factory "github.com/0xsequence/go-sequence/contracts/gen/v1/walletfactory"
+	v1Estimator "github.com/0xsequence/go-sequence/contracts/gen/v1/walletgasestimator"
+	v1Guest "github.com/0xsequence/go-sequence/contracts/gen/v1/walletguest"
+	v1Main "github.com/0xsequence/go-sequence/contracts/gen/v1/walletmain"
+	v1Upgradable "github.com/0xsequence/go-sequence/contracts/gen/v1/walletupgradable"
+	v1Utils "github.com/0xsequence/go-sequence/contracts/gen/v1/walletutils"
+	v2Factory "github.com/0xsequence/go-sequence/contracts/gen/v2/walletfactory"
+	v2Estimator "github.com/0xsequence/go-sequence/contracts/gen/v2/walletgasestimator"
+	v2Guest "github.com/0xsequence/go-sequence/contracts/gen/v2/walletguest"
+	v2Main "github.com/0xsequence/go-sequence/contracts/gen/v2/walletmain"
+	v2Upgradable "github.com/0xsequence/go-sequence/contracts/gen/v2/walletupgradable"
+	v2Utils "github.com/0xsequence/go-sequence/contracts/gen/v2/walletutils"
+	v3Factory "github.com/0xsequence/go-sequence/contracts/gen/v3/walletfactory"
+	v3Guest "github.com/0xsequence/go-sequence/contracts/gen/v3/walletguest"
+	v3Stage1 "github.com/0xsequence/go-sequence/contracts/gen/v3/walletstage1"
+	v3Stage2 "github.com/0xsequence/go-sequence/contracts/gen/v3/walletstage2"
 )
 
 var (
@@ -82,31 +82,31 @@ var (
 )
 
 func init() {
-	V1.WalletFactory = artifact("WALLET_FACTORY", walletfactory1.WalletFactoryABI, walletfactory1.WalletFactoryBin)
-	V1.WalletMainModule = artifact("WALLET_MAIN", walletmain1.WalletMainABI, walletmain1.WalletMainBin)
-	V1.WalletMainModuleUpgradable = artifact("WALLET_UPGRADABLE", walletupgradable1.WalletUpgradableABI, walletupgradable1.WalletUpgradableBin)
-	V1.WalletGuestModule = artifact("WALLET_GUEST", walletguest1.WalletGuestABI, walletguest1.WalletGuestBin)
-	V1.WalletUtils = artifact("WALLET_UTILS", walletutils1.WalletUtilsABI, walletutils1.WalletUtilsBin)
-	V1.WalletRequireFreshSigner = artifact("WALLET_REQUIRE_FRESH_SIGNER", walletutils1.WalletRequireFreshSignerABI, walletutils1.WalletRequireFreshSignerBin)
-	V1.WalletGasEstimator = artifact("WALLET_GAS_ESTIMATOR", walletgasestimator1.WalletGasEstimatorABI, walletgasestimator1.WalletGasEstimatorBin, walletgasestimator1.WalletGasEstimatorDeployedBin)
+	V1.WalletFactory = artifact("WALLET_FACTORY", v1Factory.WalletFactoryABI, v1Factory.WalletFactoryBin)
+	V1.WalletMainModule = artifact("WALLET_MAIN", v1Main.WalletMainABI, v1Main.WalletMainBin)
+	V1.WalletMainModuleUpgradable = artifact("WALLET_UPGRADABLE", v1Upgradable.WalletUpgradableABI, v1Upgradable.WalletUpgradableBin)
+	V1.WalletGuestModule = artifact("WALLET_GUEST", v1Guest.WalletGuestABI, v1Guest.WalletGuestBin)
+	V1.WalletUtils = artifact("WALLET_UTILS", v1Utils.WalletUtilsABI, v1Utils.WalletUtilsBin)
+	V1.WalletRequireFreshSigner = artifact("WALLET_REQUIRE_FRESH_SIGNER", v1Utils.WalletRequireFreshSignerABI, v1Utils.WalletRequireFreshSignerBin)
+	V1.WalletGasEstimator = artifact("WALLET_GAS_ESTIMATOR", v1Estimator.WalletGasEstimatorABI, v1Estimator.WalletGasEstimatorBin, v1Estimator.WalletGasEstimatorDeployedBin)
 	V1.CreationCode = hexutil.MustDecode("0x603a600e3d39601a805130553df3363d3d373d3d3d363d30545af43d82803e903d91601857fd5bf3")
 
-	V2.WalletFactory = artifact("WALLET_FACTORY", walletfactory2.WalletFactoryABI, walletfactory2.WalletFactoryBin)
-	V2.WalletMainModule = artifact("WALLET_MAIN", walletmain2.WalletMainABI, walletmain2.WalletMainBin)
-	V2.WalletMainModuleUpgradable = artifact("WALLET_UPGRADABLE", walletupgradable2.WalletUpgradableABI, walletupgradable2.WalletUpgradableBin)
-	V2.WalletGuestModule = artifact("WALLET_GUEST", walletguest2.WalletGuestABI, walletguest2.WalletGuestBin)
-	V2.WalletUtils = artifact("WALLET_UTILS", walletutils2.WalletUtilsABI, walletutils2.WalletUtilsBin)
-	V2.WalletGasEstimator = artifact("WALLET_GAS_ESTIMATOR", walletgasestimator2.WalletGasEstimatorABI, walletgasestimator2.WalletGasEstimatorBin, walletgasestimator2.WalletGasEstimatorDeployedBin)
+	V2.WalletFactory = artifact("WALLET_FACTORY", v2Factory.WalletFactoryABI, v2Factory.WalletFactoryBin)
+	V2.WalletMainModule = artifact("WALLET_MAIN", v2Main.WalletMainABI, v2Main.WalletMainBin)
+	V2.WalletMainModuleUpgradable = artifact("WALLET_UPGRADABLE", v2Upgradable.WalletUpgradableABI, v2Upgradable.WalletUpgradableBin)
+	V2.WalletGuestModule = artifact("WALLET_GUEST", v2Guest.WalletGuestABI, v2Guest.WalletGuestBin)
+	V2.WalletUtils = artifact("WALLET_UTILS", v2Utils.WalletUtilsABI, v2Utils.WalletUtilsBin)
+	V2.WalletGasEstimator = artifact("WALLET_GAS_ESTIMATOR", v2Estimator.WalletGasEstimatorABI, v2Estimator.WalletGasEstimatorBin, v2Estimator.WalletGasEstimatorDeployedBin)
 	V2.CreationCode = hexutil.MustDecode("0x603a600e3d39601a805130553df3363d3d373d3d3d363d30545af43d82803e903d91601857fd5bf3")
 
-	V3.WalletFactory = artifact("WALLET_FACTORY", walletfactory3.WalletFactoryABI, walletfactory3.WalletFactoryBin)
-	V3.WalletStage1Module = artifact("WALLET_STAGE_1", walletstage13.WalletStage1ABI, walletstage13.WalletStage1Bin)
-	V3.WalletStage2Module = artifact("WALLET_STAGE_2", walletstage23.WalletStage2ABI, walletstage23.WalletStage2Bin)
-	V3.WalletGuestModule = artifact("WALLET_GUEST", walletguest3.WalletGuestABI, walletguest3.WalletGuestBin)
+	V3.WalletFactory = artifact("WALLET_FACTORY", v3Factory.WalletFactoryABI, v3Factory.WalletFactoryBin)
+	V3.WalletStage1Module = artifact("WALLET_STAGE_1", v3Stage1.WalletStage1ABI, v3Stage1.WalletStage1Bin)
+	V3.WalletStage2Module = artifact("WALLET_STAGE_2", v3Stage2.WalletStage2ABI, v3Stage2.WalletStage2Bin)
+	V3.WalletGuestModule = artifact("WALLET_GUEST", v3Guest.WalletGuestABI, v3Guest.WalletGuestBin)
 	V3.CreationCode = hexutil.MustDecode("0x603e600e3d39601e805130553df33d3d34601c57363d3d373d363d30545af43d82803e903d91601c57fd5bf3")
 
 	// TODO: Update w/ v3 gas estimator
-	V3.WalletGasEstimator = artifact("WALLET_GAS_ESTIMATOR", walletgasestimator2.WalletGasEstimatorABI, walletgasestimator2.WalletGasEstimatorBin, walletgasestimator2.WalletGasEstimatorDeployedBin)
+	V3.WalletGasEstimator = artifact("WALLET_GAS_ESTIMATOR", v2Estimator.WalletGasEstimatorABI, v2Estimator.WalletGasEstimatorBin, v2Estimator.WalletGasEstimatorDeployedBin)
 
 	GasEstimator = artifact("GAS_ESTIMATOR", gasestimator.GasEstimatorABI, gasestimator.GasEstimatorBin, gasestimator.GasEstimatorDeployedBin)
 
