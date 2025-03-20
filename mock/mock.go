f4e23fe096e75b252176ba7ac8f88a6fd1148de0
Alex Guerrieri <ag@horizon.io>
2025-03-20 13:43:20 +0100

    remove imports alias

--- mock/mock.go
+++ mock/mock.go
@@ -1,7 +1,7 @@
 package mock
 
-//go:generate go run go.uber.org/mock/mockgen -imports proto=../api -destination api.mock.go -package mock  -mock_names API=API ../api API
-//go:generate go run go.uber.org/mock/mockgen -imports proto=../indexer -destination indexer.mock.go -package mock  -mock_names Indexer=Indexer ../indexer Indexer
-//go:generate go run go.uber.org/mock/mockgen -imports proto=../marketplace -destination marketplace.mock.go -package mock  -mock_names Marketplace=Marketplace ../marketplace Marketplace
-//go:generate go run go.uber.org/mock/mockgen -imports proto=../metadata -destination metadata.mock.go -package mock  -mock_names Metadata=Metadata ../metadata Metadata
-//go:generate go run go.uber.org/mock/mockgen -imports proto=../relayer/proto -destination relayer.mock.go -package mock  -mock_names Relayer=Relayer ../relayer/proto Relayer
+//go:generate go run go.uber.org/mock/mockgen -destination api.mock.go -package mock  -mock_names API=API ../api API
+//go:generate go run go.uber.org/mock/mockgen -destination indexer.mock.go -package mock  -mock_names Indexer=Indexer ../indexer Indexer
+//go:generate go run go.uber.org/mock/mockgen -destination marketplace.mock.go -package mock  -mock_names Marketplace=Marketplace ../marketplace Marketplace
+//go:generate go run go.uber.org/mock/mockgen -destination metadata.mock.go -package mock  -mock_names Metadata=Metadata ../metadata Metadata
+//go:generate go run go.uber.org/mock/mockgen -destination relayer.mock.go -package mock  -mock_names Relayer=Relayer ../relayer/proto Relayer
