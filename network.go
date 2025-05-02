bf7dba1ccd8aab04fbdd490b2187abf14dfa9f77
Shun Kakinoki <shunkakinoki@gmail.com>
2025-05-03 00:16:56 +0900

    Update GuestModuleAddress in network context and contract artifacts
    This commit updates the GuestModuleAddress in the network context and the corresponding contract artifacts to a new address. The changes ensure consistency across the codebase and align with the latest deployment configurations. Additionally, the WalletGuestMetaData has been updated to reflect the new bytecode and ABI, enhancing the contract's integration with the system.

--- network.go
+++ network.go
@@ -76,7 +76,7 @@ var sequenceContextV3 = WalletContext{
 	FactoryAddress:              common.HexToAddress("0xBd0F8abD58B4449B39C57Ac9D5C67433239aC447"),
 	MainModuleAddress:           common.HexToAddress("0x53bA242E7C2501839DF2972c75075dc693176Cd0"),
 	MainModuleUpgradableAddress: common.HexToAddress("0xa29874c88b8Fd557e42219B04b0CeC693e1712f5"),
-	GuestModuleAddress:          common.HexToAddress("0x9cbB2a4BD361248f5020465E1Cc1Db877F9387D8"),
+	GuestModuleAddress:          common.HexToAddress("0x6aE2a1Bb476CF8C50ab09499b99Ec36056c53B6D"),
 	// SessionManagerAddress:       common.HexToAddress("0xDfB66323C6485eE10d81A0fa60BaEbbbA732Ba0a"),
 	UtilsAddress: common.HexToAddress("0x53bA242E7C2501839DF2972c75075dc693176Cd0"),
 	CreationCode: hexutil.Encode(contracts.V3.CreationCode),
