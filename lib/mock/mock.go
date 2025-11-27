c7ba1d97735eb870dd0a3bf7ebe7cd0c8677b771
David Sedláček <david.littlefarmer@gmail.com>
2025-11-27 14:37:43 +0100

    update indexer mock

--- lib/mock/mock.go
+++ lib/mock/mock.go
@@ -1,7 +1,7 @@
 package mock
 
 //go:generate go run go.uber.org/mock/mockgen -destination api.mock.go -package mock  -mock_names API=API ../../services/api API
-//go:generate go run go.uber.org/mock/mockgen -destination indexer.mock.go -package mock -mock_names Indexer=Indexer,IndexerClient=IndexerClient,IndexerGateway=IndexerGateway,IndexerGatewayClient=IndexerGatewayClient ../../services/indexer Indexer,IndexerClient,IndexerGateway,IndexerGatewayClient
+//go:generate go run go.uber.org/mock/mockgen -destination indexer.mock.go -package mock -mock_names IndexerClient=IndexerClient,IndexerGatewayClient=IndexerGatewayClient ../../services/indexer IndexerClient,IndexerGatewayClient
 //go:generate go run go.uber.org/mock/mockgen -destination marketplace.mock.go -package mock  -mock_names Marketplace=Marketplace ../../services/marketplace Marketplace
 //go:generate go run go.uber.org/mock/mockgen -destination metadata.mock.go -package mock  -mock_names MetadataClient=Metadata ../../services/metadata MetadataClient
 //go:generate go run go.uber.org/mock/mockgen -destination relayer.mock.go -package mock  -mock_names RelayerClient=Relayer ../../relayer/proto RelayerClient
