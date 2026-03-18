f485629d3e1f5ae2f99d791a36f5c54041ea64bc
Michael Standen <mstan@horizon.io>
2026-03-18 13:28:22 +1300

    Support sapient signatures in intent_config (#351)

--- contracts/gen/gen.go
+++ contracts/gen/gen.go
@@ -37,6 +37,8 @@
 //go:generate go run github.com/0xsequence/ethkit/cmd/ethkit abigen --pkg=walletestimator --type=WalletEstimator --outFile=./v3/walletestimator/wallet_estimator.gen.go --artifactsFile=../artifacts/wallet-contracts-v3/Estimator.sol/Estimator.json --includeDeployedBin
 //go:generate go run github.com/0xsequence/ethkit/cmd/ethkit abigen --pkg=walletsimulator --type=WalletSimulator --outFile=./v3/walletsimulator/wallet_simulator.gen.go --artifactsFile=../artifacts/wallet-contracts-v3/Simulator.sol/Simulator.json --includeDeployedBin
 
+//go:generate go run github.com/0xsequence/ethkit/cmd/ethkit abigen --pkg=sapientmock --type=SapientMock --outFile=./sapientmock/sapient_mock.gen.go --artifactsFile=../artifacts/wallet-contracts-v3/MockSapient.sol/MockSapient.json
+
 //
 // tokens
 //
