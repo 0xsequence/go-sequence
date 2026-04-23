5e28fc2f1cc036701e692c2f3e94db6244152ae8
Patryk Kalinowski <pkalinowski@polygon.technology>
2026-04-23 19:50:56 +0200

    relayer: allow authorizations with chainId=0

--- relayer/relayer.go
+++ relayer/relayer.go
@@ -248,7 +248,8 @@ func (r *Client) Relay(ctx context.Context, signedTxs *sequence.SignedTransactio
 
 	var authorization *proto.EIP7702Authorization
 	if signedTxs.Authorization != nil {
-		if signedTxs.ChainID.Uint64() != signedTxs.Authorization.ChainID.Uint64() {
+		chainID := signedTxs.Authorization.ChainID.Uint64()
+		if chainID != 0 && signedTxs.ChainID.Uint64() != chainID {
 			return "", nil, nil, fmt.Errorf("chain ID mismatch between signed transactions and authorization")
 		}
 
@@ -257,7 +258,7 @@ func (r *Client) Relay(ctx context.Context, signedTxs *sequence.SignedTransactio
 		signedTxs.Authorization.S.WriteToSlice(sig[32:64])
 		sig[64] = signedTxs.Authorization.V
 		authorization = &proto.EIP7702Authorization{
-			ChainId:        signedTxs.Authorization.ChainID.Uint64(),
+			ChainId:        chainID,
 			Nonce:          signedTxs.Authorization.Nonce,
 			Implementation: signedTxs.Authorization.Address.Hex(),
 			Signature:      hexutil.Encode(sig[:]),
@@ -307,7 +308,8 @@ func (r *Client) FeeOptions(ctx context.Context, signedTxs *sequence.SignedTrans
 
 	var authorization *proto.EIP7702Authorization
 	if signedTxs.Authorization != nil {
-		if signedTxs.ChainID.Uint64() != signedTxs.Authorization.ChainID.Uint64() {
+		chainID := signedTxs.Authorization.ChainID.Uint64()
+		if chainID != 0 && signedTxs.ChainID.Uint64() != chainID {
 			return nil, nil, fmt.Errorf("chain ID mismatch between signed transactions and authorization")
 		}
 
@@ -316,7 +318,7 @@ func (r *Client) FeeOptions(ctx context.Context, signedTxs *sequence.SignedTrans
 		signedTxs.Authorization.S.WriteToSlice(sig[32:64])
 		sig[64] = signedTxs.Authorization.V
 		authorization = &proto.EIP7702Authorization{
-			ChainId:        signedTxs.Authorization.ChainID.Uint64(),
+			ChainId:        chainID,
 			Nonce:          signedTxs.Authorization.Nonce,
 			Implementation: signedTxs.Authorization.Address.Hex(),
 			Signature:      hexutil.Encode(sig[:]),
